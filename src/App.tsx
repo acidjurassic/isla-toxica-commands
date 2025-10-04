56f13852039c80c3a67ec793982d1b813f05c6e5
acidjurassic <acidjurassic@gmail.com>
2025-10-04 18:44:40 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import AcidJurassicClicker from "./AcidJurassicClicker";
-
+import AcidJurassicClicker from "./AcidJurassicClicker"; // or "./components/AcidJurassicClicker" if that's your path
 
 export default function App() {
   return (
-    <main className="w-full max-w-6xl p-6">
+    <main className="w-full max-w-6xl p-6 bg-red-900"> {/* <-- TEMP test color */}
       <AcidJurassicClicker />
     </main>
   );
