81a9f72910570915e13258aef3421bf81aceab0a
acidjurassic <acidjurassic@gmail.com>
2025-10-05 03:33:32 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './index.css';
-import AcidJurassicClicker from './components/AcidJurassicClicker';
+import "./index.css";
+import AcidJurassicClicker from "./AcidJurassicClicker";
 
 export default function App() {
   return (
-    <main className="w-full max-w-6xl p-6 bg-red-900"> {/* TEMP: should appear dark red */}
+    <main className="w-full max-w-6xl p-6 bg-red-900">
       <AcidJurassicClicker />
     </main>
   );
 }
+
 
