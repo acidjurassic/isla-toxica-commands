4d53ffb1633a6c065fa97be6bdca11f56d50b335
acidjurassic <acidjurassic@gmail.com>
2025-10-05 03:37:04 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./index.css";
-import AcidJurassicClicker from "./AcidJurassicClicker";
+import AcidJurassicClicker from "./AcidJurassicClicker"; // or "./components/AcidJurassicClicker"
 
 export default function App() {
   return (
-    <main className="w-full max-w-6xl p-6 bg-red-900">
-      <AcidJurassicClicker />
-    </main>
+    <div className="min-h-screen bg-gray-900 text-gray-100 flex items-start justify-center">
+      {/* top spacing so header isn’t glued to the top */}
+      <main className="w-full max-w-4xl px-4 sm:px-6 lg:px-8 pt-10 pb-16">
+        <AcidJurassicClicker />
+      </main>
+    </div>
   );
 }
+
 
 
